e4266446f87e9334ffb1a6dde2e7911b2b95bac1
Markus Mildt <59928997+markus-mildt@users.noreply.github.com>
2024-02-04 02:43:22 -0500

    added case conversion

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function convert(){
     const bbl_input = document.getElementById('bbl_input').value;
     if(!isNaN(bbl_input) && parseFloat(bbl_input) > 0){
-        const cans = (parseFloat(bbl_input) * 31 * 128) / 12;
-        document.getElementById('result').innerText = `That is about ${cans} cans.`
+        const cans = Math.floor((parseFloat(bbl_input) * 31 * 128) / 12);
+        const cases = Math.floor(cans / 24);
+        document.getElementById('cans_result').innerText = `That is about ${cans} cans and about ${cases} cases.`
     } else {
         document.getElementById.innerText = `Invalid input, please enter a positive numerical value.`
     }
