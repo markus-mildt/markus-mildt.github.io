5eced9ac2ad7d8f799e43882ab7b43dbfa7f7ce3
Markus Mildt <59928997+markus-mildt@users.noreply.github.com>
2024-02-04 22:53:44 -0500

    Removed error hander
    removed error hander for now, will probably add back later

--- index.js
+++ index.js
@@ -4,7 +4,5 @@ function convert(){
         const cans = Math.floor((parseFloat(bbl_input) * 31 * 128) / 12);
         const cases = Math.floor(cans / 24);
         document.getElementById('cans_result').innerText = `That is about ${cans} cans and about ${cases} cases.`
-    } else {
-        document.getElementById.innerText = `Invalid input, please enter a positive numerical value.`
-    }
+    } 
 }
